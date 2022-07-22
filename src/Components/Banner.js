52f585893c622ab4af4547fdf032b434d27a1ca8
omaima ouahline <ouahline.omaima@ine.inpt.ma>
2022-07-22 14:00:24 +0100

    test commit

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -10,6 +10,7 @@ export const Banner = () => {
                         <span className="tagline">WebDev is my word</span>
                         <h1>{"Hi I'm OUAHLINE Omaima"} <span className="wrap"> web developer</span></h1>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum quos eaque nemo tenetur neque ullam quidem nulla. Ut, nihil officia. Rem culpa rerum illum, totam illo animi perspiciatis aut? Exercitationem.</p>
+                        <p>test commit</p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
                     </Col>
                     <Col xs={12} md ={60} xl ={5}>
