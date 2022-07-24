ada43f38a29b4ecb1ba551f0174480301d833fae
omaima ouahline <ouahline.omaima@ine.inpt.ma>
2022-07-24 23:50:33 +0100

    working on skills section

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,8 +1,100 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import {Container, Row, Col} from "react-bootstrap";
+import meter1 from '../assets/img/meter1.svg'
+import meter2 from '../assets/img/meter2.svg'
+import meter3 from '../assets/img/meter3.svg'
+import colorSharp from '../assets/img/color-sharp.png'
 export const Skills = () => {
-    return ( 
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+        };
+        const style1 = {"--clr": "rgb(74, 47, 189)", "--num": 85}
+        const style2 = {"--clr": "rgb(74, 47, 189)", "--num": 90} 
+        const style3 = {"--clr": "rgb(74, 47, 189)", "--num": 9} 
+        const style4 = {"--clr": "rgb(74, 47, 189)", "--num": 90}  
+    return (
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className="skill-bx">
+                        <h2>Skills</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ducimus aliquid eaque, sunt velit temporibus nesciunt voluptatibus delectus, est porro praesentium fugiat quo minus dignissimos, nisi autem recusandae expedita aspernatur!</p>
+                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            {/* <div className="item">
+                                <img src={meter1} alt="image" />
+                                <h5>Web Developpement</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="image" />
+                                <h5>UX/UI Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="image" />
+                                <h5>Desktop Developpement</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="image" />
+                                <h5>Design Thinking</h5>
+                            </div> */}
+                            
+                                <div className="percent" style={style1}>
+                                    <div className="dot">
 
-     );
+                                    </div>
+                                    <svg>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                    </svg>
+                                </div>
+                  
+                            {/* 
+                                <div className="percent" style={style2} >
+                                    <svg>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                    </svg>
+                                </div>
+                            
+                            
+                                <div className="percent" style={style3}>
+                                    <svg>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                    </svg>
+                                </div>
+                            
+                            
+                                <div className="percent" style={style4} >
+                                    <svg>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                        <circle cx="65" cy="65" r="65"></circle>
+                                    </svg>
+                                </div> */}
+                           
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp} alt="" />
+        </section>
+      );
 }
-
